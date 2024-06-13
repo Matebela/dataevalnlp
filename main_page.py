2a732d7449f6a0e7e30148a8c59f6010ff9dd6cf
Matebela <matebela.it@gmail.com>
2024-06-13 13:05:15 -0400

    Delete main_page.py

--- main_page.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def display():
-    st.title("Main Page")
-    st.write("Welcome to the app! This app helps you manage various tasks.")
-    st.write("Use the sidebar to navigate to different task pages.")
