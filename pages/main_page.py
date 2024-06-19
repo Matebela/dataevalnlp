41d8a76b0fcf291dfd161bdca36e6185a57f0aa7
Matebela <matebela.it@gmail.com>
2024-06-19 17:45:10 -0400

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def display():
     st.title("Main Page")
-    st.write("Welcome to the app! This app helps you manage various tasks.")
-    st.write("Use the sidebar to navigate to different task pages.")
+    st.write("Welcome to the beta app! This app helps you manage various tasks.")
+    st.write("Use the sidebar dropdown menu to navigate to different task pages.")
